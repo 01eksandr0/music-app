9264c8c8f98334897eeb5744bea28f0cc9d859b8
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-02-25 16:30:55 +0200

    1

---
+++ src/components/HomeHero/HomeHero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import css from "./HomeHero.module.css";
+import { FaHeart } from "react-icons/fa";
+
+export const HomeHero = () => {
+  return (
+    <section className={css.hero}>
+      <div className={css.heroContainer}>
+        <div>
+          <img
+            className={css.backImg}
+            src="https://variety.com/wp-content/uploads/2022/07/Music-Streaming-Wars.jpg?w=1024"
+            alt=""
+          />
+        </div>
+        <ul className={css.list}>
+          <li>
+            <div className={css.heroElement}>
+              <div className={css.itemImgCont}>
+                <div className={css.itemImg}>
+                  <FaHeart className={css.icon} />
+                </div>
+              </div>
+              <p className={css.itemText}>My favorite</p>
+            </div>
+          </li>
+          <li>
+            <div className={css.heroElement}></div>
+          </li>
+          <li>
+            <div className={css.heroElement}></div>
+          </li>
+          <li>
+            <div className={css.heroElement}></div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import css from "./HomePage.module.css";
 import { RecomendTracks } from "../../components/RecomendTracks/RecomendTracks";
+import { HomeHero } from "../../components/HomeHero/HomeHero";
 
 export const HomePage = () => {
   return (
     <div className={css.home}>
-      <section className={css.hero}>
-        <div>
-          {/*  */}
-          <div style={{ display: "flex", gap: 20 }}>
-            <img src="" alt="" width={686} height={373} />
-            <ul style={{ display: "flex", gap: 38, flexDirection: "column" }}>
-              <li>
-                <img src="" alt="" width={320} height={96} />
-              </li>
-              <li>
-                <img src="" alt="" width={320} height={96} />
-              </li>
-              <li>
-                <img src="" alt="" width={320} height={96} />
-              </li>
-            </ul>
-          </div>
-          {/*  */}
-          <RecomendTracks />
-        </div>
-      </section>
+      <HomeHero />
+      <RecomendTracks />
     </div>
   );
 };
