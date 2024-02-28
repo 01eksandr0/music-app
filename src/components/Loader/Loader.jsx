10ddf5eedb4ba2aa155198bc477cd09080765cb4
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-02-28 17:11:11 +0200

    1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,17 +3,30 @@ import { LineWave } from "react-loader-spinner";
 
 export const Loader = () => {
   return (
-    <LineWave
-      visible={true}
-      height="200"
-      width="200"
-      color="#4fa94d"
-      ariaLabel="line-wave-loading"
-      wrapperStyle={{ margin: "10px 100px" }}
-      wrapperClass=""
-      firstLineColor="#fff"
-      middleLineColor="#fff"
-      lastLineColor="#fff"
-    />
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        position: "fixed",
+        alignItems: "center",
+        height: "100%",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <LineWave
+        visible={true}
+        height="200"
+        width="200"
+        color="#4fa94d"
+        ariaLabel="line-wave-loading"
+        wrapperStyle={{ margin: "10px auto" }}
+        wrapperClass=""
+        firstLineColor="#fff"
+        middleLineColor="#fff"
+        lastLineColor="#fff"
+      />
+    </div>
   );
 };
