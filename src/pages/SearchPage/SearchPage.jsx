81f8191b4a759c06eb69fe67d89006dc3b422598
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-03-03 14:11:06 +0200

    1

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import css from "./SearchPage.module.css";
 import { Link } from "react-router-dom";
+import phototrack from "../../../public/img/photosong.png";
+import photoartist from "../../../public/img/photoartist.jpeg";
 
 export const SearchPage = () => {
   return (
@@ -9,22 +11,13 @@ export const SearchPage = () => {
       <div className={css.list}>
         <Link to="/search/track">
           <div className={css.box}>
-            <img
-              src="../../../public/img/photosong.png"
-              width={260}
-              height={260}
-            />
+            <img src={phototrack} width={260} height={260} />
             <p className={css.text}> Track</p>
           </div>
         </Link>
         <Link to="/search/artist">
           <div className={css.boxSecond}>
-            <img
-              src="../../../public/img/photoartist.jpeg"
-              alt=""
-              width={400}
-              height={250}
-            />
+            <img src={photoartist} alt="" width={400} height={250} />
             <p className={css.text}>Artist</p>
           </div>
         </Link>
